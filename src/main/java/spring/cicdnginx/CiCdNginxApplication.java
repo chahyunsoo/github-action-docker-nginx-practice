b8bf1e72f852dfdc04a3dc7e34d26d4039513d01
chahyunsoo <chahs1012@naver.com>
2024-03-19 09:04:07 +0900

    [Feat] : first commit

---
+++ src/main/java/spring/cicdnginx/CiCdNginxApplication.java
@@ -0,0 +1,13 @@
+package spring.cicdnginx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdNginxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdNginxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/cicdnginx/CiCdNginxApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.cicdnginx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdNginxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
